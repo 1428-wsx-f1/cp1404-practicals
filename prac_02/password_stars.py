a4f944e37281b5fa02d844ffbf7088d990278f71
1428-wsx-f1 <abailey3981@gmail.com>
2023-09-24 23:43:47 +1000

    Added docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,17 +2,20 @@ MINIMUM_PASSWORD_LENGTH = 5
 
 
 def main():
+    """Program to get and display password"""
     password = get_password()
     display_password(password)
 
 
 def display_password(password):
+    """Displays the password as *'s"""
     print("*" * len(password))
 
 
 def get_password():
+    """Gets the password and determines if it is long enough"""
     password = input("Enter A Password: ")
-    while len(password) < MINIMUM_PASSWORD_LENGTH:
+    while len(password) <= MINIMUM_PASSWORD_LENGTH:
         print("Password Isn't Long Enough")
         password = input("Enter A Password: ")
     return password
