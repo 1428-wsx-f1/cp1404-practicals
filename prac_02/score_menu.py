34c1f3b997d3f4f8fadf498f492cf33ddebbc2af
1428-wsx-f1 <abailey3981@gmail.com>
2023-09-24 23:54:17 +1000

    Added docstring

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,12 +1,13 @@
 
 def main():
+    """Program to get valid score, print result and print result as *'s"""
     print("(G)et valid score\n(P)rint result\n(S)how stars\n(Q)uit")
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
             get_valid_score()
         elif choice == "P":
-            print(f"{get_valid_score(score)}")
+            print(f"{get_valid_score()}")
         elif choice == "S":
             print("*" * get_valid_score())
         else:
@@ -25,13 +26,6 @@ def get_valid_score():
     return score
 
 
-def display_score(score):
-    """Function to get a valid score between 0 and 100"""
-    score = int(input("Enter Score: "))
-    while score < 0 or score > 100:
-        print("Invalid Score")
-        score = int(input("Enter Score: "))
-    return score
 
 
 main()
