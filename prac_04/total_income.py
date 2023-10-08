6079b3e673a0a2f4f191ae8d528a0214a0e0b4f1
1428-wsx-f1 <abailey3981@gmail.com>
2023-10-08 17:55:59 +1000

    Replace format method with f-string, Improve variable naming

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,7 +5,7 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of months."""
+    """Generate income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
 
@@ -13,12 +13,16 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    display_income_report(incomes)
+
+
+def display_income_report(incomes):
+    """Calculate the total income and display the income report"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes, 1):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
