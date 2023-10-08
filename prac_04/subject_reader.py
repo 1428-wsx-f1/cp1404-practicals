2e4144aa78cb70aab2e6a96a2fa966f97cbc6494
1428-wsx-f1 <abailey3981@gmail.com>
2023-10-08 22:01:20 +1000

    Improve function to get subjects, Add function to display function data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,12 +7,15 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    """Read data from a file and display the data"""
+    subject_data = get_subjects()
+    print(subject_data)
+    display_subject_data(subject_data)
 
 
-def get_data():
+def get_subjects():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subjects = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +25,16 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subjects.append(parts)
         print("----------")
     input_file.close()
+    return subjects
+
+
+def display_subject_data(subject_data):
+    """Display the subject data"""
+    for subject in subject_data:
+        print(f"{subject[0]} is taught by {subject[1]:13} and has {subject[2]:3} students.")
 
 
 main()
