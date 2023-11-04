3488b500dcf476a7b9e95008c5cedf4dd19163a4
1428-wsx-f1 <abailey3981@gmail.com>
2023-11-04 23:03:46 +1000

    Add load_guitars function, Refactor my_guitars.py to use csv reader

--- prac_07/my_guitars.py
+++ prac_07/my_guitars.py
@@ -5,22 +5,27 @@ Actual:  minutes
 """
 
 from prac_07.guitar import Guitar
+import csv
 
 FILENAME = "guitars.csv"
 
 
 def main():
     """Read guitar file and save guitars objects to display."""
-    guitars = []
-    with open(FILENAME, 'r', newline='') as in_file:
-        in_file.readline()
-        for line in in_file:
-            parts = line.strip().split(',')
-            guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-            guitars.append(guitar)
+    print("My Guitars!")
+    guitars = load_guitars(FILENAME)
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+
+
+def load_guitars(file_name):
+    guitars = []
+    with open(FILENAME, 'r', newline='') as in_file:
+        reader = csv.reader(in_file)
+        for row in reader:
+            guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
+    return guitars
 
 
 main()
