4a5acbbbf5dc3bf181ebad706563c879ef17b934
1428-wsx-f1 <abailey3981@gmail.com>
2023-11-04 23:08:37 +1000

    Add get_guitars function to my_guitars.py

--- prac_07/my_guitars.py
+++ prac_07/my_guitars.py
@@ -14,6 +14,7 @@ def main():
     """Read guitar file and save guitars objects to display."""
     print("My Guitars!")
     guitars = load_guitars(FILENAME)
+    guitars = get_guitars(guitars)
     guitars.sort()
     for guitar in guitars:
         print(guitar)
@@ -24,7 +25,20 @@ def load_guitars(file_name):
     with open(FILENAME, 'r', newline='') as in_file:
         reader = csv.reader(in_file)
         for row in reader:
+            # Name, Year, Cost
             guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
+    return guitars
+
+
+def get_guitars(guitars):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: $"))
+        guitar = Guitar(name, int(year), cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name: ")
     return guitars
 
 
