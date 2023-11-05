232fbf92ed15ae429ea1f106d6577a3696b6e921
1428-wsx-f1 <abailey3981@gmail.com>
2023-11-05 13:29:11 +1000

    Add project.py

---
+++ prac_07/project.py
@@ -0,0 +1,19 @@
+
+class Project:
+    """Project class to storing details for projects."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise project class."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        """Return project details."""
+        return (f"{self.name}, Start: {self.start_date.strftime('%d/%m/%Y')}, Priority {self.priority}, "
+                f"Estimate ${self.cost_estimate:,.2f}, Completion: {self.completion_percentage}%")
+
+    def __lt__(self, other):
+        return self.priority < other.priority
