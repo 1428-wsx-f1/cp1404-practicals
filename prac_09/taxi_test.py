f8e179e1fb543d881c339e70b3839a76c03892bc
1428-wsx-f1 <abailey3981@gmail.com>
2023-11-18 12:50:05 +1000

    Complete Class Variables excercise

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -9,7 +9,7 @@ from prac_09.taxi import Taxi
 
 def main():
     """Code to show use of taxi class."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.start_fare()
     my_taxi.drive(40)
     print(my_taxi)
